ff7bceb672f973b247bb65907318dc20c893420a
fabio-k <fabiokmain@gmail.com>
2025-02-22 14:11:08 -0300

    initial commit

---
+++ src/main/java/org/fatec/esportiva/EsportivaApplication.java
@@ -0,0 +1,13 @@
+package org.fatec.esportiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportivaApplication.class, args);
+	}
+
+}
