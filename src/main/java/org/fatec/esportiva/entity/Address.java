3f422968012fc3c8676c6e605381ed79c430fd3e
Lucas-K <kazuomizomenezes@gmail.com>
2025-03-05 15:35:08 -0300

    feat: Adding .css

--- src/main/java/org/fatec/esportiva/entity/Address.java
+++ src/main/java/org/fatec/esportiva/entity/Address.java
@@ -3,14 +3,10 @@ package org.fatec.esportiva.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import org.fatec.esportiva.entity.enums.AddressType;
 import org.fatec.esportiva.entity.enums.ResidencyType;
 import org.fatec.esportiva.entity.enums.StreetType;
-import org.hibernate.annotations.Cascade;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
