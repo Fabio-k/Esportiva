0e441b78724e650a87a325d0f02677d3f7b2d9dc
fabio-k <fabiokmain@gmail.com>
2025-03-13 14:15:43 -0300

    refactor: tests to be visible when running them

--- src/test/java/org/fatec/esportiva/e2e/E2E.java
+++ src/test/java/org/fatec/esportiva/e2e/E2E.java
@@ -4,7 +4,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.time.Duration;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -27,26 +29,52 @@ public class E2E {
         WebDriverManager.chromedriver().setup();
     }
 
-    @BeforeEach
-    void setup() {
-        browser = new ChromeDriver(new ChromeOptions().addArguments("--headless"));
+    @BeforeAll
+    static void setup() {
+        browser = new ChromeDriver();
         wait = new WebDriverWait(browser, Duration.ofSeconds(15));
     }
 
-    @AfterEach
-    void teardown() {
-        browser.quit();
+    @AfterAll
+    static void teardown(){
+        browser.close();
+    }
+
+    public void authenticateAs(String name) {
+        wait.until(ExpectedConditions.urlContains("/login"));
+        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("submitButton")));
+        selectByText(By.id("users"), name);
+
+        submitButton.click();
+
+        wait.until(ExpectedConditions.stalenessOf(submitButton));
+    }
+
+    public WebElement waitUntilId(String id){
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.id(id)));
+        sleep();
+        return element;
     }
 
     public void fillInputField(By locator, String value) {
         WebElement inputField = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
         inputField.clear();
         inputField.sendKeys(value);
+        sleep();
     }
 
     public void selectByText(By locator, String visibleText) {
         WebElement selectField = wait.until(ExpectedConditions.elementToBeClickable(locator));
         Select select = new Select(selectField);
         select.selectByVisibleText(visibleText);
+        sleep();
+    }
+
+    public void sleep(){
+        try{
+            Thread.sleep(1000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 }
