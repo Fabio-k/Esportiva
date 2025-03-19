89b5e3104c15477422c242f7d0d1f9294b4dab67
Lucas-K <kazuomizomenezes@gmail.com>
2025-03-19 20:47:45 -0300

    fix: ProductDTO attributes to camel case

--- src/main/java/org/fatec/esportiva/request/ProductDto.java
+++ src/main/java/org/fatec/esportiva/request/ProductDto.java
@@ -27,19 +27,19 @@ public class ProductDto {
 
     @NotNull(message = "Data de entrada não pode ficar em branco")
     @PastOrPresent(message = "Data de entrada pode ser até o presente momento")
-    private Date entry_date;
+    private Date entryDate;
 
     @NotNull(message = "Quantidade no estoque não pode ficar em branco")
     @Min(value = 0, message = "Quantidade no estoque não pode ser negativa")
-    private int stock_quantity;
+    private int stockQuantity;
 
     @NotNull(message = "Quantidade reservada pelos carrinhos de compra não pode ficar em branco")
     @Min(value = 0, message = "Quantidade reservada pelos carrinhos de compra não pode ser negativa")
-    private int blocked_quantity;
+    private int blockedQuantity;
 
     @NotNull(message = "Margem de lucro não pode ficar em branco")
     @Min(value = 0, message = "Margem de lucro deve ser maior que 0%")
-    private float pricing_value;
+    private float pricingValue;
 
     @NotNull(message = "Custo não pode ficar em branco")
     @Min(value = 0, message = "O custo não pode ser negativo")
@@ -47,15 +47,15 @@ public class ProductDto {
 
     // Pode ter justificativa vazia (Quando não está desativado, por exemplo)
     @NotNull(message = "A justificativa de desativação não pode ser nula, mas pode ser uma string vazia")
-    private String deactivation_justification;
+    private String deactivationJustification;
 
     @NotNull(message = "Categoria de desativação não pode ficar em branco")
-    private ProductStatus deactivation_category;
+    private ProductStatus deactivationCategory;
 
     @NotNull(message = "Referencia para uma categoria de inativação não pode ficar em branco")
-    private Long pricing_group_id;
+    private Long pricingGroupId;
 
     @NotNull(message = "Referencia a uma categoria de produto não pode ficar em branco")
     @Default
-    private List<ProductCategory> product_category = new ArrayList<>();
+    private List<ProductCategory> productCategory = new ArrayList<>();
 }
