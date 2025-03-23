e58d83f16a2b7bea2bb28b354e40f2105f049d06
Lucas-K <kazuomizomenezes@gmail.com>
2025-03-23 13:59:07 -0300

    feat: Partial log listener

---
+++ src/main/java/org/fatec/esportiva/listeners/LogListener.java
@@ -0,0 +1,60 @@
+package org.fatec.esportiva.listeners;
+
+import java.time.LocalDateTime;
+
+import org.fatec.esportiva.entity.Log;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreRemove;
+import jakarta.persistence.PreUpdate;
+import jakarta.transaction.Transactional;
+
+public class LogListener {
+
+    // Injetará o EntityManager configurado automaticamente no contexto do Spring
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @PrePersist
+    public void beforeInsert(Object entity) {
+        Log log = new Log();
+        // log.setUser();
+        log.setOperation("INSERT");
+        log.setOperationContent(entity.toString());
+        log.setTimestamp(LocalDateTime.now());
+
+        salvarLog(log);
+    }
+
+    @PreUpdate
+    public void beforeUpdate(Object entity) {
+        Log log = new Log();
+        // log.setUser();
+        log.setOperation("UPDATE");
+        log.setOperationContent(entity.toString());
+        log.setTimestamp(LocalDateTime.now());
+
+        salvarLog(log);
+    }
+
+    // @PreRemove
+    // public void beforeDelete(Object entity) {
+    // Log log = new Log();
+    // log.setUser();
+    // log.setOperation("DELETE");
+    // log.setOperationContent(entity.toString());
+    // log.setTimestamp(LocalDateTime.now());
+
+    // salvarLog(log);
+    // }
+
+    // Use EntityManager para salvar o log no banco de dados
+    // @ Transactional -> O Spring gerencia a transação automaticamente, sem
+    // necessidade de chamar manualmente begin() ou commit()
+    @Transactional
+    private void salvarLog(Log log) {
+        entityManager.persist(log);
+    }
+}
