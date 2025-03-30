b1c38a63391597a3d2058b4035d0625fda0644b1
fabio-k <fabiokmain@gmail.com>
2025-03-29 23:37:20 -0300

    fix: remove unnecessary host from fetch urls and add negative quantity validation

--- src/main/java/org/fatec/esportiva/service/ProductService.java
+++ src/main/java/org/fatec/esportiva/service/ProductService.java
@@ -52,6 +52,7 @@ public class ProductService {
     }
 
     public CartItem updateQuantity(Long id, Short quantity) {
+        if(quantity < 0) throw new IllegalArgumentException("Quantidade deve ser maior do que zero");
         Product product = findById(id);
         Integer availableStock = product.getStockQuantity() - product.getBlockedQuantity();
         if(availableStock < quantity){
@@ -67,6 +68,7 @@ public class ProductService {
     }
 
     public void returnBlockedProductQuantity(Long id, Short quantity){
+        if(quantity < 0) throw new IllegalArgumentException("Quantidade deve ser maior do que zero");
         Product product = findById(id);
         if(product.getBlockedQuantity() < quantity) throw new IllegalArgumentException("Erro no estoque");
         product.setBlockedQuantity(product.getBlockedQuantity() - quantity);
