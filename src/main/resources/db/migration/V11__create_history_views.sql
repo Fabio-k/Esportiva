28022a5751836176591e3b4760a5136243eee75a
fabio-k <fabiokmain@gmail.com>
2025-04-26 13:38:11 -0300

    feat: add materialized ProductHistory view and remove unnecessary argument form transactionService generateTransaction

---
+++ src/main/resources/db/migration/V11__create_history_views.sql
@@ -0,0 +1,15 @@
+CREATE MATERIALIZED VIEW historico_compras AS
+SELECT
+    row_number() OVER () as id,
+    c.cat_id,
+    c.cat_nome,
+    pro.pro_id,
+    pro.pro_nome_produto,
+    t.tra_data_compra, SUM(p.ped_quantidade) AS total_de_compras
+    FROM transacoes t
+    JOIN pedidos p ON t.tra_id = p.ped_tra_id
+    JOIN produtos pro ON pro.pro_id = p.ped_pro_id
+    JOIN pertence per ON per.per_pro_id = pro.pro_id
+    JOIN categorias_produto c ON c.cat_id = per.per_cat_id
+    GROUP BY  c.cat_id, c.cat_nome, pro.pro_id, pro.pro_nome_produto, t.tra_data_compra
+    ORDER BY t.tra_data_compra, c.cat_id;
