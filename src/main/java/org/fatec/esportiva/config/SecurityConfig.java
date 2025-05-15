c1d791fd8c2582591a85749facc99a18d67c52e3
fabio-k <fabiokmain@gmail.com>
2025-05-15 16:10:51 -0300

    test: Add test to product management

--- src/main/java/org/fatec/esportiva/config/SecurityConfig.java
+++ src/main/java/org/fatec/esportiva/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -33,10 +32,13 @@ public class SecurityConfig {
                                                 .requestMatchers("/admin/**").hasRole("ADMIN")
                                                 .requestMatchers("/h2-console/**").permitAll()
                                                 .anyRequest().authenticated())
-                                .formLogin(login -> login
-                                                .loginPage("/login")
-                                                .permitAll())
-                                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-                                        .build();
+                                                .formLogin(login -> login
+                                                    .loginPage("/login")
+                                                    .permitAll()
+                                                )
+                        .exceptionHandling(
+                                exception -> exception
+                                        .accessDeniedHandler(((request, response, accessDeniedException) -> response.sendRedirect("/login"))))
+                        .build();
         }
 }
