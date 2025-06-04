36eeba0316042674e451a65a5c1baef7b68ddebd
Lucas-K <kazuomizomenezes@gmail.com>
2025-06-04 15:30:49 -0300

    feat: Adding notes

--- src/main/java/org/fatec/esportiva/service/ProductHistoryService.java
+++ src/main/java/org/fatec/esportiva/service/ProductHistoryService.java
@@ -19,12 +19,16 @@ public class ProductHistoryService {
     private final ProductHistoryRepository productHistoryRepository;
 
     public SalesHistoryResponseDto getCategoryOrProductHistoryById(Long id, Boolean isCategory, LocalDate startDate, LocalDate endDate) {
+        // Na ausência de datas, seleciona a data de hoje até o dia seguinte como faixa de valores
         LocalDateTime endDateTime = null, startDateTime = null;
         if (endDate != null){
             endDateTime = LocalDateTime.of(endDate.plusDays(1), LocalTime.MIN);
         }
-        if(startDate != null)
+        if(startDate != null){
             startDateTime = startDate.atStartOfDay();
+        }
+        
+        // Obtém os dados do BD para encaminhar ao front-end
         List<CategoryProductHistoryView> salesHistoryByDate =  productHistoryRepository.getCategoryOrProductHistoryById(id, isCategory, startDateTime , endDateTime, OrderStatus.getSalesReportStatus());
         List<CategoryProductStateView> salesHistoryByState = productHistoryRepository.getCategoryOrProductStateHistoryById(id, isCategory, startDateTime, endDateTime, OrderStatus.getSalesReportStatus());
         SalesHistoryResponseDto salesHistoryResponseDto = new SalesHistoryResponseDto();
